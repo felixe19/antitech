3f0592b39c7d12f47df7ef8f24a82d263d402c1a
felixe19 <erlindafelixe@gmail.com>
2023-12-12 18:32:15 +0000

    updated

--- create_database.sql
+++ create_database.sql
@@ -27,7 +27,7 @@ CREATE TABLE user (
     email VARCHAR(100) NOT NULL,
     pwd VARCHAR(100) NOT NULL,
     -- profile picture ? maybe
-    Bio TEXT
+    bio TEXT
 );
 
 # book / library table
@@ -40,4 +40,26 @@ CREATE TABLE book (
     isAvailable BOOLEAN
 );
 
-# TODO >> hardcode info into these two : user & book !
+
+# basic values - TODO : ADD INFO > use full table
+INSERT INTO book (title, author)
+VALUES
+    ('Abolish Silicon Valley: How to Liberate Technology from Capitalism', 'Wendy Liu'),
+    ('Algorithms of Oppression: How Search Engines Reinforce Racism', 'Safiya Umoja Noble'),
+    ('Do Androids Dream of Electric Cars?: Public Transit in the Age of Google, Uber, and Elon Musk', 'James Wilt'),
+    ('Too Smart: How Digital Capitalism is Extracting Data, Controlling Our Lives, and Taking Over the World', 'Jathan Sadowski'),
+    ('Bit Tyrants: The Political Economy of Silicon Valley', 'Rob Larson'),
+    ('Capitalism, Technology, Labor: Socialist Register Reader Vol 2 (Socialist Register Reader, 2)', 'Greg Albo'),
+    ('Race After Technology: Abolitionist Tools for the New Jim Code', 'Ruha Banjamin'),
+    ('Weapons of Math Destruction: How Big Data Increases Inequality and Threatens Democracy', 'Cathy O''Neil'),
+    ('Automating Inequality: How High-Tech Tools Profile, Police, and Punish the Poor', 'Virginia Eubanks');
+
+# basic values >> pwds with the $2a$10$ bycrypt prefix ...
+INSERT INTO user (username, email, pwd, bio) VALUES
+    ('frank001', 'frank@example.com', '$2a$10$yBs5h5Ql48kLlIR9pY/XfuF3iv3Zp6EsF.HNmyfFw/8AhBntoUu0C', 'hi my name is frank'),
+    ('edith436', 'edith@example.com', '$2a$10$FOAsAp0hqoD1tEGT2vS8oOJ7TZrD7Sx0zZ5Qt5NlgFv8E3VDlPlDG', 'hi my name is edith'),
+    ('_ma_maria98', 'maria@example.com', '$2a$10$Cw/Mha7p5Kx1gt8nNPYp.e1Z.kMg/aHm0Xt4/HFrsLwJkzy3jn2mG', 'holi soy maria~'),
+    ('noor_31', 'noor@example.com', '$2a$10$4y4D7no5Pnp33hhbVDEbDexmvOxP.B.DZLFaMQD4cvnUpJ/VD9P/2', 'im noor :)'),
+    ('sakur4_', 'sakura@example.com', '$2a$10$LKRP57T9U0GRzbpCz8p5VucOqRmE6ng6CTn/gvGjK9DK5f8QQzF16', 'hi im sakura.x');
+
+# TODO : finish tables >> blog & community
