9feed3ec71eddc586740178e6022ad650fd7fc5d
felixe19 <erlindafelixe@gmail.com>
2023-12-12 18:05:47 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # antitech
+Databases ATW mini project
--- create_database.sql
+++ create_database.sql
@@ -19,3 +19,25 @@ DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS community;
 
 # create the tables
+
+# user table
+CREATE TABLE user (
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    pwd VARCHAR(100) NOT NULL,
+    -- profile picture ? maybe
+    Bio TEXT
+);
+
+# book / library table
+CREATE TABLE book (
+    bookID INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    descript TEXT,
+    genre VARCHAR(50),
+    isAvailable BOOLEAN
+);
+
+# TODO >> hardcode info into these two : user & book !
